e14fb605b7c37aa9947888a08848b9102182f56d
kxkxzzz <kxkxzzznya@gmail.com>
2025-09-09 21:07:01 +0800

    配置clang-fromat

---
+++ Framework/Interface/IRuntimeModule.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Interface.hpp"
+
+namespace kx {
+class IRuntimeModule {
+public:
+    virtual int Initialize() = 0;
+    virtual void Tick() = 0;
+    virtual void Shutdown() = 0;
+};
+}  // namespace kx
